d5520a18e1924b9a9b6ec2f7f3dffb3e2eb40b14
Laura Porsch <laupor112@gmail.com>
2023-07-01 11:08:13 -0600

    added subtasks, added flash messages in case Category already exists

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import asc, desc
 from sqlalchemy.orm import relationship
+from sqlalchemy.exc import IntegrityError
 import os
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField, SelectField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, DateField, SelectField
+from wtforms.validators import DataRequired, ValidationError
 import datetime as dt
 
 # ---------------------------- START FLASK FRAMEWORK ------------------------------- #
@@ -35,28 +36,28 @@ class Categories(db.Model):
 class ToDos(db.Model):
     __tablename__ = "to_dos"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), unique=True, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
     parent_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
     parent_category = relationship("Categories", back_populates="to_dos_category")
     due_date = db.Column(db.Date)
-    sub_to_dos = relationship("SubToDos", back_populates="parent_to_do")
+    subtasks = relationship("SubTasks", back_populates="parent_to_do")
 
     def __repr__(self):
         """returns name of ToDo when printed instead of <ToDos ID>"""
         return self.name
 
 
-class SubToDos(db.Model):
-    __tablename__ = "sub_to_dos"
+class SubTasks(db.Model):
+    __tablename__ = "subtasks"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), unique=True, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
     due_date = db.Column(db.Date)
     to_do_id = db.Column(db.Integer, db.ForeignKey("to_dos.id"))
-    parent_to_do = relationship("ToDos", back_populates="sub_to_dos")
-    category = db.Column(db.String(250), unique=True)
+    parent_to_do = relationship("ToDos", back_populates="subtasks")
+    category = db.Column(db.String(250))
 
     def __repr__(self):
-        """returns name of SubToDos when printed instead of <SubToDos ID>"""
+        """returns name of SubTasks when printed instead of <SubToDos ID>"""
         return self.name
 
 
@@ -87,6 +88,12 @@ class GotDones(db.Model):
 
 
 # ---------------------------- CREATE FORMS ------------------------------- #
+# create custom validator so due dates can't be in the past
+def date_check(form, field):
+    if field.data < dt.datetime.now().date():
+        raise ValidationError("Due Date can't be in the past!")
+
+
 class CategoryForm(FlaskForm):
     name = StringField('Category Name', validators=[DataRequired()])
     submit = SubmitField('Add Category')
@@ -97,25 +104,32 @@ class UpdateCategoryForm(FlaskForm):
     submit = SubmitField('Update Category')
 
 
+class SubTaskForm(FlaskForm):
+    name = StringField('Subtask Name', validators=[DataRequired()])
+    due_date = DateField('Due Date', validators=[DataRequired(), date_check])
+    submit = SubmitField('Add Subtask')
+
+
 # ---------------------------- CREATE ROUTES ------------------------------- #
 @app.route('/')
 def dashboard():
     """renders index.html, calls all ToDos saved in the DB and shows an overview of the ToDo"""
     to_dos = db.session.query(ToDos).order_by(asc(ToDos.due_date))
+    print(to_dos)
     return render_template('index.html', all_to_dos=to_dos)
 
 
 @app.route('/add-new-to-do', methods=["GET", "POST"])
 def add_new_to_do():
     """renders 'add-new-to-do.html', if form is filled out and submitted, gets data from user input and saves it as new
-    ToDo in the DB, redirects to show-to-do.html"""
+    To Do in the DB, redirects to show-to-do.html"""
     categories = db.session.query(Categories).all()
 
     # ToDoForm setup withing function to be able to call new categories to choose from
     class ToDoForm(FlaskForm):
         name = StringField('To Do Name', validators=[DataRequired()])
         category = SelectField('Category', choices=categories, validators=[DataRequired()])
-        due_date = DateField('Due Date', validators=[DataRequired()])
+        due_date = DateField('Due Date', validators=[DataRequired(), date_check])
         submit = SubmitField('Add To Do')
     form = ToDoForm()
     if form.validate_on_submit():
@@ -133,7 +147,7 @@ def add_new_to_do():
 
 @app.route('/show-to-do/<int:to_do_id>')
 def show_to_do(to_do_id):
-    """renders show-to-do.html, calls requested ToDo from the DB and shows all info"""
+    """renders show-to-do.html, calls requested To Do from the DB and shows all info"""
     requested_to_do = db.session.get(ToDos, to_do_id)
     return render_template('show-to-do.html', to_do=requested_to_do)
 
@@ -161,8 +175,12 @@ def add_new_category():
         new_category = Categories(
             name=form.name.data
         )
-        db.session.add(new_category)
-        db.session.commit()
+        try:
+            db.session.add(new_category)
+            db.session.commit()
+
+        except IntegrityError:
+            flash("This Category already exists!")
         return redirect(url_for('show_category', category=new_category.name))
     else:
         return render_template('add-new-category.html', form=form)
@@ -177,7 +195,8 @@ def got_done():
 
 @app.route('/mark-done/<int:to_do_id>')
 def mark_done(to_do_id):
-    """calls requested to_do from ToDos DB, deletes it and saves it in GotDones DB"""
+    """calls requested to_do from ToDos DB and all related SubTasks, saves them in GotDones DB, redirects to delete.html
+    """
     requested_to_do = db.session.get(ToDos, to_do_id)
     new_got_done = GotDones(
         name=requested_to_do.name,
@@ -186,13 +205,27 @@ def mark_done(to_do_id):
     )
     db.session.add(new_got_done)
     db.session.commit()
+    subtasks = requested_to_do.subtasks
+    if subtasks:
+        for subtask in subtasks:
+            new_got_done = GotDones(
+                name=subtask.name,
+                category=str(subtask.category),
+                date=dt.datetime.now().date()
+            )
+            db.session.add(new_got_done)
+            db.session.commit()
     return redirect(url_for('delete', to_do_id=to_do_id))
 
 
 @app.route('/delete/<int:to_do_id>')
 def delete(to_do_id):
-    """deletes requested to_do from ToDos DB"""
+    """deletes requested To Do and all related SubTasks from DB"""
     requested_to_do = db.session.get(ToDos, to_do_id)
+    subtasks = requested_to_do.subtasks
+    for subtask in subtasks:
+        db.session.delete(subtask)
+        db.session.commit()
     db.session.delete(requested_to_do)
     db.session.commit()
     return redirect(url_for('dashboard'))
@@ -208,6 +241,49 @@ def update_category(category_id):
         return redirect(url_for('show_category', category=category_update))
     else:
         return render_template('update-category.html', form=form, category=category_update)
+
+
+@app.route('/add-new-subtask/<int:to_do_id>', methods=["GET", "POST"])
+def add_new_subtask(to_do_id):
+    """renders 'add-new-subtask.html', if form is filled out and submitted, gets data from user input and saves it as new
+    Subtask of the requested To Do to the DB, redirects to show-to-do.html"""
+    form = SubTaskForm()
+    if form.validate_on_submit():
+        to_do = db.session.get(ToDos, to_do_id)
+        new_subtask = SubTasks(
+            name=form.name.data,
+            due_date=form.due_date.data,
+            parent_to_do=to_do,
+            category=str(to_do.parent_category))
+        db.session.add(new_subtask)
+        db.session.commit()
+        return redirect(url_for('show_to_do', to_do_id=to_do.id))
+    else:
+        return render_template('add-new-subtask.html', form=form)
+
+
+@app.route('/mark-done-sub/<int:subtask_id>')
+def mark_done_sub(subtask_id):
+    """calls requested subtask from SubTasks DB, deletes it and saves it in GotDones DB"""
+    requested_subtask = db.session.get(SubTasks, subtask_id)
+    new_got_done = GotDones(
+        name=requested_subtask.name,
+        category=str(requested_subtask.category),
+        date=dt.datetime.now().date()
+    )
+    db.session.add(new_got_done)
+    db.session.commit()
+    return redirect(url_for('delete_sub', subtask_id=subtask_id))
+
+
+@app.route('/delete_sub/<int:subtask_id>')
+def delete_sub(subtask_id):
+    """deletes requested subtask from SubTasks DB"""
+    requested_subtask = db.session.get(SubTasks, subtask_id)
+    to_do_id = requested_subtask.to_do_id
+    db.session.delete(requested_subtask)
+    db.session.commit()
+    return redirect(url_for('show_to_do', to_do_id=to_do_id))
 
 
 if __name__ == "__main__":
